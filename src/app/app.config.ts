dde5b856feb7f7de6b0b493e5d841844bcbf4a00
Abdelrahim.Aref <Abdelrahim.Aref@hadafsolutions.com>
2026-01-13 14:00:58 +0200

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { coreProviders } from './core/core.providers';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { routes } from './app.routes';  // your main routes
-import { sectorsRoutes } from './features/sectors/sectors.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    ...coreProviders,
-    provideRouter(
-      [
-        ...routes,
-        ...sectorsRoutes, 
-      ],
-      withHashLocation() 
-    ),
-  ],
+    ...coreProviders
+  ]
 };
